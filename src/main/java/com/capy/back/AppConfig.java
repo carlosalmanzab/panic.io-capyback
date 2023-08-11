010ab8a43d280cc8d28c64f6297eab7a9e848bda
Carlos Almanza Bedoya <64765773+carlosalmanzab@users.noreply.github.com>
2023-08-11 13:44:56 -0500

    se agrega addCors config

--- src/main/java/com/capy/back/AppConfig.java
+++ src/main/java/com/capy/back/AppConfig.java
@@ -3,9 +3,21 @@ package com.capy.back;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AppConfig {
+@EnableWebMvc
+public class AppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")//para todos los endpoints
+                .allowedOrigins("http://localhost:8100")//permitir solicitudes de todos los origenes
+                .allowedMethods("*")//permitir todos los metodos
+                .allowCredentials(true);
+    }
 
     @Bean
     public ModelMapper modelMapper() {
