043798ad71a4821edd05a073622c20a6fc2ad1a6
savito03 <savioromero2019@gmail.com>
2023-09-23 16:37:29 -0500

    se corrige esa vaina mala

--- src/main/java/com/capy/back/localizacion/service/impl/ComunaService.java
+++ src/main/java/com/capy/back/localizacion/service/impl/ComunaService.java
@@ -3,7 +3,9 @@ package com.capy.back.localizacion.service.impl;
 import com.capy.back.localizacion.model.dto.ComunaDTO;
 import com.capy.back.localizacion.model.dto.ComunaOutDTO;
 import com.capy.back.localizacion.model.entity.Comuna;
+import com.capy.back.localizacion.model.mapper.BarrioMapperService;
 import com.capy.back.localizacion.model.mapper.ComunaMapperService;
+import com.capy.back.localizacion.repository.IBarrioRepository;
 import com.capy.back.localizacion.repository.IComunaRepository;
 import com.capy.back.localizacion.service.IComunaService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +21,13 @@ public class ComunaService implements IComunaService {
     IComunaRepository comunaRepository;
 
     @Autowired
+    IBarrioRepository barrioRepository;
+
+    @Autowired
     ComunaMapperService comunaMapperService;
+
+    @Autowired
+    BarrioMapperService barrioMapperService;
 
     @Override
     public ComunaOutDTO listarComunas() {
@@ -30,7 +38,14 @@ public class ComunaService implements IComunaService {
             comunaOutDTO.setMensajeError("No se encontraron comunas");
             return comunaOutDTO;
         }
+
         List<ComunaDTO> comunaDTOList = comunaList.stream().map(comunaMapperService::toDTO).collect(Collectors.toList());
+
+        //llenar barrios por comuna
+        comunaDTOList.forEach(comunaDTO -> {
+            comunaDTO.setBarrios(barrioRepository.findAllByIdComuna(comunaDTO.getId()).stream().map(barrioMapperService::toDTO).collect(Collectors.toList()));
+        });
+
         comunaOutDTO.setComunaDTOList(comunaDTOList);
         comunaOutDTO.setTotalComunas((long) comunaDTOList.size());
         return comunaOutDTO;
